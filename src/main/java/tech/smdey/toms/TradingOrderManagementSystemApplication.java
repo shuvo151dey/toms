f654e32df10c10f59ca1d3d0276d480d96eae7fc
shuvo151dey <shuvo151dey@gmail.com>
2024-12-31 09:56:59 +0000

    Spring boot initializr

---
+++ src/main/java/tech/smdey/toms/TradingOrderManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package tech.smdey.toms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingOrderManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingOrderManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/smdey/toms/TradingOrderManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.smdey.toms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingOrderManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
